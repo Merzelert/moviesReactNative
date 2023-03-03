4cc884310707ea8c6ee114bd76fa1d9d8c14dd8e
Erik Ivan Retana Lopez <merzelert@gmail.com>
2023-03-03 14:21:05 -0600

    delete

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const movieDB = axios.create({
     baseURL: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: '83e57ad893fac390af3ad21fd2e1e73d',
+        api_key: 'API_KEY',
         language: 'en-US',
     },
 });
